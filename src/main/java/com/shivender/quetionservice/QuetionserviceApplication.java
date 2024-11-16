54fe61688b513840554030d91acdd9184e12cc64
Shivender Kumar <shivendra@plutos.one>
2024-11-16 23:09:07 +0530

    init question micro service

---
+++ src/main/java/com/shivender/quetionservice/QuetionserviceApplication.java
@@ -0,0 +1,13 @@
+package com.shivender.quetionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuetionserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuetionserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shivender/quetionservice/QuetionserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivender.quetionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuetionserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
